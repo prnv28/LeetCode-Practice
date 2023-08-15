717c0bd75595d4ab5f5e0084d40ca9cf339a154d
Pranav Gajera <pranavgajera1998@gmail.com>
2023-08-15 12:14:18 +0530

    Time: 15 ms (96.39%), Space: 12.8 MB (98.74%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    bool detectCycle(int i,int parent,int vis[],int dfsVis[],vector<int> adj[]){
-        vis[i] = 1;
-        dfsVis[i] = 1;
-        for(int adjacent : adj[i]){
-            if(!vis[adjacent]){
-                if(detectCycle(adjacent,i,vis,dfsVis,adj)) return true;
-            }else if(dfsVis[adjacent]){
-                return true;
-            }
-        }
-        dfsVis[i]=0;
-        return false;
-    }
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> adj[numCourses];
@@ -20,17 +7,28 @@ public:
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
 
-        int vis[numCourses];
-        int dfsVis[numCourses];
-        memset(vis,0,sizeof vis);
-        memset(dfsVis,0,sizeof dfsVis);
+        int indegree[numCourses];
+        memset(indegree,0,sizeof indegree);
 
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(detectCycle(i,-1,vis,dfsVis,adj)) return false;
+            for(int j=0;j<adj[i].size();j++){
+                indegree[adj[i][j]]++;
             }
         }
-        return true;
-        
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        int count = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            count++;
+            for(int adjacent : adj[node]){
+                indegree[adjacent]--;
+                if(indegree[adjacent]==0) q.push(adjacent);
+            }
+        }
+        return count==numCourses?true:false;
     }
 };
