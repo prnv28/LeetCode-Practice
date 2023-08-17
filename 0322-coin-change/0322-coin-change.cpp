36d304a6cdbff9e53a3b0c85cf17a2e5b2cc1ff2
Pranav Gajera <pranavgajera1998@gmail.com>
2023-08-17 18:14:33 +0530

    Time: 84 ms (71.36%), Space: 18.7 MB (43.51%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    // long algo(int i,int amount,vector<int> &coins){
-    //     if(amount<=0) return 0;
-    //     if(i<0 && amount>0) return INT_MAX;
-
-    //     if(coins[i]<=amount){
-    //         return min(algo(i-1,amount,coins),algo(i-1,amount%coins[i],coins)+(amount/coins[i]));
-    //     }else{
-    //         return algo(i-1,amount,coins);
-    //     }
-    // }
-    // int coinChange(vector<int>& coins, int amount) {
-    //     int n = coins.size();
-    //     int ans = (int)algo(n-1,amount,coins);
-    //     return ans==INT_MAX?-1:ans;
-    // }
-
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size() ;
-       vector<int> dp(amount+1,0) ; 
-       for (int j  = 0; j < amount +1 ; j++){
-            dp[j] = (j == 0) ? 0 : INT_MAX - 1;
-       }
-        vector<int> curr = dp ; 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < amount + 1; j++) {
-                if (coins[i - 1] > j)
+        vector<int> dp(amount+1, INT_MAX-1);
+        dp[0] = 0;
+        int n = coins.size();
+        vector<int> curr = dp;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<amount+1;j++){
+                if(coins[i-1]>j){
                     curr[j] = 0 + dp[j];
-                else
-                    curr[j] = min(0 + dp[j], 1+curr[j-coins[i-1]]);
+                }else{
+                    curr[j] = min(dp[j],1+curr[j-coins[i-1]]);
+                }
             }
-            dp = curr; 
+            dp = curr;
         }
-        return (dp[amount]==INT_MAX-1)?-1:dp[amount];
-        
+
+        return dp[amount]==INT_MAX-1?-1:dp[amount];
     }
 };
