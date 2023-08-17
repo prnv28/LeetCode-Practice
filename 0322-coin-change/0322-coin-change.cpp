6360caa7d0d0a536dc5e952631ddb87ea7eb81cc
Pranav Gajera <pranavgajera1998@gmail.com>
2023-08-17 19:00:28 +0530

    Time: 47 ms (96.59%), Space: 14.4 MB (83.85%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,6 +3,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1);
         dp[0] = 0;
+        sort(coins.begin(),coins.end());
         for(int i=1;i<amount+1;i++){
             dp[i] = amount+1;
             for(int c : coins){
