1cebe65d021e140629703ac182f601e67b85bd36
Pranav Gajera <pranavgajera1998@gmail.com>
2023-08-17 19:04:18 +0530

    Time: 18 ms (99.78%), Space: 10 MB (98.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1);
+        int dp[++amount];
         dp[0] = 0;
-        sort(coins.begin(),coins.end());
-        for(int i=1;i<amount+1;i++){
-            dp[i] = amount+1;
-            for(int c : coins){
-                if((i-c)<0) break;
-                if(dp[i - c] != amount+1)   dp[i] = min(dp[i],1+dp[i-c]);
+        sort(begin(coins), end(coins));
+        for (int i = 1; i < amount; i++) {
+            dp[i] = INT_MAX;
+            for (int c: coins) {
+                if (i - c < 0) break;
+                if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
             }
         }
-
-        return dp[amount]==amount+1?-1:dp[amount];
+        return dp[--amount] == INT_MAX ? -1 : dp[amount];
     }
 };
