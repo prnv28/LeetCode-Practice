532ead7055640ce9c95b68fd45678d4be99cb9ba
Pranav Gajera <pranavgajera1998@gmail.com>
2023-08-18 00:50:58 +0530

    Time: 216 ms (77.31%), Space: 94.1 MB (50.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-    bool algo(int i, int target, vector<int> &nums,vector<vector<int>>& dp){
-        
-        if(target==0) return 1;
-        if(i==0){
-            return nums[i]==target;
-        }
 
-        if(dp[i][target]!=-1) return dp[i][target];
-
-        int notTaken = algo(i-1,target,nums,dp);
-        int taken = 0;
-        if(target>=nums[i]){
-            taken = algo(i-1,target-nums[i],nums,dp);
-        }
-
-        return dp[i][target] = (notTaken || taken);
-    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int target = accumulate(nums.begin(),nums.end(),0);
         if(target%2 || n<2) return false;
         target = target/2;
-        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0] = 1;
-        }
+
+        vector<int> prev(target+1,0);
+        prev[0] = 1;
+        if(nums[0]<=target)
+            prev[nums[0]] = 1;
+        
         for(int i=1;i<n;i++){
+            vector<int> curr(target+1,0);
+            curr[0] = 1;
             for(int j=1;j<target+1;j++){
                 if(j>=nums[i]){
-                    dp[i][j] = dp[i-1][j-nums[i]] || dp[i-1][j];
+                    curr[j] = prev[j-nums[i]] || prev[j];
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
-                
             }
+            prev = curr;
         }
-        return dp[n-1][target];
+        return prev[target];
         
     }
 };
