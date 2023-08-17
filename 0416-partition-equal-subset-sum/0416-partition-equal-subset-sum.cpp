6c7e0ae52b023917a04e01a82968fc50f672f33b
Pranav Gajera <pranavgajera1998@gmail.com>
2023-08-18 01:03:27 +0530

    Time: 105 ms (92.15%), Space: 9.5 MB (97.81%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,28 +3,20 @@ public:
 
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        int target = accumulate(nums.begin(),nums.end(),0);
-        if(target%2 || n<2) return false;
-        target = target/2;
+        int target = 0;
+        for(int num : nums) target+=num;
+        if((target & 1) || n<2) return false;
+        target = target >> 1;
 
-        vector<int> prev(target+1,0);
-        prev[0] = 1;
-        if(nums[0]<=target)
-            prev[nums[0]] = 1;
-        
-        for(int i=1;i<n;i++){
-            vector<int> curr(target+1,0);
-            curr[0] = 1;
-            for(int j=1;j<target+1;j++){
-                if(j>=nums[i]){
-                    curr[j] = prev[j-nums[i]] || prev[j];
-                }else{
-                    curr[j] = prev[j];
-                }
+        int dp[target+1];
+        for(int i=0;i<target+1;i++) dp[i]= 0;
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] = dp[j] || dp[j-nums[i]];
             }
-            prev = curr;
         }
-        return prev[target];
+        return dp[target];
         
     }
 };
