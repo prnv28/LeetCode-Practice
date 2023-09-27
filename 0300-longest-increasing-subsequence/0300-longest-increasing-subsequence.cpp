119deed9fcfe84f35433f5177a59bf3c8183f576
Pranav Gajera <pranavgajera1998@gmail.com>
2023-09-27 12:41:57 +0530

    Time: 3 ms (98.71%), Space: 10.9 MB (37.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,16 +5,14 @@ public:
         vector<int> data;
         data.reserve(n);
         data.push_back(nums[0]);
-        int count = 1;
         for(int i=1;i<n;i++){
             if(data.back()<nums[i]){
                 data.push_back(nums[i]);
-                count++;
             }else{
                 int index = lower_bound(data.begin(),data.end(),nums[i]) - data.begin();
                 data[index] = nums[i];
             }
         }
-        return count;
+        return data.size();
     }
 };
