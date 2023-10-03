4a031ad4d6daa2d270fd6f6057d5eaf7cdb7d061
Pranav Gajera <pranavgajera1998@gmail.com>
2023-10-04 00:08:22 +0530

    Time: 3 ms (37.96%), Space: 6.5 MB (8.62%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        n = (n >> 16) | (n << 16);
-        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
-        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
-        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
-        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
-        return n;
+       int result = 0;
+        for(int i = 0; i<32; i++){
+            int lsb = n & 1;
+            int res = lsb << (31-i);
+            result = result | res;
+            n = n >> 1;
+        }
+        return result;
     }
 };
