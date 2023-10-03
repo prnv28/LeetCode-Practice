27b2381c249cc4e65af177107d56b43acf437605
Pranav Gajera <pranavgajera1998@gmail.com>
2023-10-04 00:25:09 +0530

    Time: 3 ms (99.22%), Space: 11.3 MB (15.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,9 +6,8 @@ public:
             int rem = target - nums[i];
             if(mp.find(rem)!=mp.end()){
                 return {mp[rem],i};
-            }else{
-                mp[nums[i]] = i;
             }
+            mp[nums[i]] = i;
         }
         return {-1,-1};
     }
