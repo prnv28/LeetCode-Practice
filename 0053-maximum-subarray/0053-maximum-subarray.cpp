5ad259ca459ad53f179e1dd5b88e7658d6eb18fc
Pranav Gajera <pranavgajera1998@gmail.com>
2023-10-28 18:36:05 +0530

    Time: 53 ms (99.54%), Space: 68 MB (65.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         if(nums.size()==0) return 0;
-        // if(nums.size()==1 && nums[0]<0) return 0;
-
         int maxsum = 0;
         int globalmax = INT_MIN;
-
         for(int n : nums){
             maxsum+= n;
             globalmax = max(globalmax,maxsum);
