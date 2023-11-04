dd7ba6e88e525f973661e1aebbacce86449bc5f8
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-04 18:23:00 +0530

    Time: 16 ms (99.36%), Space: 20.2 MB (77.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,14 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
         unordered_map<string,vector<string>> mp;
-        for(auto a : strs){
-            string s = a; 
-            sort(s.begin(),s.end());
-            if(mp.find(s)==mp.end()){
-                mp[s]= {a};
-            }else{
-                mp[s].push_back(a);
-            }
+        for(int i=0;i<strs.size();i++){
+            string key = strs[i];
+            sort(key.begin(),key.end());
+            mp[key].emplace_back(move(strs[i]));
         }
 
         for(auto itr = mp.begin();itr!=mp.end();itr++){
