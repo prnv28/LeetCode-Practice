f27630258f65eaec3f3c7829676cc87cec4b30c7
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-04 18:01:23 +0530

    Time: 6 ms (80.79%), Space: 7.7 MB (63.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,12 +4,11 @@ public:
         // transform(s.begin(),s.end(),s.begin(),::tolower);
         // transform(t.begin(),t.end(),t.begin(),::tolower);
         if(s.size()!=t.size()) return false;
-        unordered_map<char,pair<int,int>> mp;
+        unordered_map<int,pair<int,int>> mp;
         for(int i=0;i<s.size();i++){
-            mp[s[i]].first++;
-            mp[t[i]].second++;
+            mp[s[i]-'a'].first++;
+            mp[t[i]-'a'].second++;
         }
-
         for(auto itr=mp.begin();itr!=mp.end();itr++){
             if(itr->second.first!=itr->second.second) return false;
         }
