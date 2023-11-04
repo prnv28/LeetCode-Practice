5c3cff3050f7d82207255a6dc09ac59131a73bc1
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-04 20:10:39 +0530

    Time: 6 ms (80.79%), Space: 7.9 MB (7.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        return false;
+        unordered_map<int,pair<int,int>> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]-'a'].first++;
+            mp[t[i]-'a'].second++;
+        }
+        for(auto itr=mp.begin();itr!=mp.end();itr++){
+            if(itr->second.first!=itr->second.second) return false;
+        }
+        return true;
     }
 };
