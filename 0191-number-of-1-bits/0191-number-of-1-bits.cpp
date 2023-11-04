fce8972572f23b611c974482e83c434ff502219b
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-04 18:37:33 +0530

    Time: 0 ms (100%), Space: 6.3 MB (46.45%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        // int count = 0;
-        // while(n!=0){
-        //     count++;
-        //     n &= (n-1);
-        // }
-        return __builtin_popcount((n));
+        int count=0;
+        while(n!=0){
+            int bit = n&1;
+            if(bit) count++;
+            n = n>>1;
+        }
+        return count;
     }
 };
