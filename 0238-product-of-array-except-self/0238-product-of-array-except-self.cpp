9b44b44f446ac0811eba96af44992bc0d8571f06
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-05 17:37:58 +0530

    Time: 19 ms (54.5%), Space: 25.4 MB (22.3%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefix(nums.size());
-        vector<int> suffix(nums.size());
-        for(int i=0;i<nums.size();i++){
+        int n = nums.size();
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        for(int i=0;i<n;i++){
             if(i==0){
                 prefix[i] = nums[i];
-                suffix[nums.size()-1-i] = nums[nums.size()-1-i];
+                suffix[n-1-i] = nums[n-1-i];
             }else{
                 prefix[i] = prefix[i-1]*nums[i];
-                suffix[nums.size()-1-i] = suffix[nums.size()-i]*nums[nums.size()-1-i]; 
+                suffix[n-1-i] = suffix[n-i]*nums[n-1-i]; 
             }
            
         }
-        vector<int> result(nums.size());
-        for(int i=0;i<nums.size();i++){
+        vector<int> result(n);
+        for(int i=0;i<n;i++){
             if(i==0) result[i] = suffix[i+1];
-            else if(i==nums.size()-1) result[i] = prefix[i-1];
+            else if(i==n-1) result[i] = prefix[i-1];
             else result[i] = prefix[i-1]*suffix[i+1];
         }
         return result;
