51ee208854a345946748af078f796b48781226f4
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-05 17:14:16 +0530

    Time: 16 ms (28.5%), Space: 13.8 MB (84.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    
-    class cmp{
-      public:
-      
-      bool operator()(pair<int,int> &a, pair<int,int>&b){
-          return a.second > b.second;
-      }
+    static 
+
+    class Compare {
+        public:
+        bool operator()(pair<int,int>& a, pair<int,int>& b){
+            return a.second>b.second;
+        }
     };
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        unordered_map<int,int> ds;
-        vector<int> result;
-        for(int i : nums){
-            ds[i]++;
-        }
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp> pq;
-        
-        for(auto itr = ds.begin();itr!=ds.end();itr++){
+        unordered_map<int,int> mp;
+        for(int num : nums) mp[num]++;
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,Compare> pq;
+
+        for(auto itr=mp.begin();itr!=mp.end();itr++){
             if(pq.size()<k){
                 pq.push(*itr);
-            }else if(pq.size()==k && (pq.top().second)<(*itr).second){
+            }else if(pq.size()==k && (pq.top().second < itr->second)){
                 pq.pop();
                 pq.push(*itr);
             }
         }
-        
-        for(int i = 0;i<k;i++){
+
+        vector<int> result;
+        result.reserve(k);
+        while(!pq.empty()){
             result.push_back(pq.top().first);
             pq.pop();
         }
-        
-        return result; 
+
+        return result;
     }
 };
