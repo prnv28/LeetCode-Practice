3e96917a0432f498ec24279ca83b0b3cdcb058ed
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-06 16:44:19 +0530

    Time: 462 ms (6.54%), Space: 181.9 MB (8.05%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -82,10 +82,3 @@ public:
         this->mp[key] = this->head->next;
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
