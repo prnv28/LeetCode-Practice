5710747375ddb9f3b7066a1a602dcb242d9df840
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-06 16:45:13 +0530

    Time: 329 ms (97.41%), Space: 165.4 MB (76.19%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,84 +1,102 @@
 class LRUCache {
 public:
-    class node{
-        public:
-            int key;
-            int val;
-            node* prev;
-            node* next;
+    // declaring our data structures
+    class Node {
+    public:
+        int key;
+        int val;
+        Node *prev;
+        Node *next;
 
-            node(int _key,int _val){
-                key = _key;
-                val = _val;
-            }
+        Node (int key, int val) {
+            this->key = key;
+            this->val = val;
+            prev = nullptr;
+            next = nullptr;
+        }
     };
 
-    node* head = new node(-1,-1);
-    node* tail = new node(-1,-1);
-    map<int,node*> mp;
-    int capacity;
+    Node *head = nullptr;
+    Node *tail = nullptr;
 
-    LRUCache(int cap) {
-        this->capacity = cap;
-        this->head->next = this->tail;
-        this->tail->prev = this->head;
+    unordered_map <int, Node*> map;
+    int cap;
+    int currSize = 0;
+
+// functions 
+    LRUCache(int capacity) {
+        cap = capacity;
     }
 
-    void addNode(node* newNode){
-        node* newNext = this->head->next;
-        newNode->next = newNext;
-        newNode->prev = this->head;
-        this->head->next = newNode;
-        newNext->prev = newNode;
+    void pushToEnd (Node *node) {
+        if (node == tail) return;
+        if (node == head) {
+            head = node->next;
+        }
+        else {
+            node->prev->next = node->next;
+        }
+        node->next->prev = node->prev;
+        node->next = nullptr;
+        tail->next = node;
+        node->prev = tail;
+        tail = node;
     }
 
-    void deleteNode(node* delNode){
-        node* delNext = delNode->next;
-        node* delPrev = delNode->prev;
-        delPrev->next = delNext;
-        delNext->prev = delPrev; 
+    void deleteLRUnode () {
+        Node *temp = head->next;
+        map.erase(head->key);
+        delete head;
+        if (!temp) {
+            head = nullptr;
+            tail = nullptr;
+        }
+        else {
+            head = temp;
+        }
+        currSize--;
+        return;
     }
+
+    void addToEnd (Node *node) {
+        map[node->key] = node;
+        if (!tail) {
+            head = node;
+            tail = node;
+        }
+        else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+        currSize++;
+    }
+
+
+// main functions to be implemented
     
-    int get(int key_) {
-        // cout<<"Get :: "<<this->mp.size()<<"\n";
-        if(this->mp.find(key_)!=this->mp.end()){
-            node* temp = this->mp[key_];
-            int res = temp->val;
-            this->mp.erase(key_);
-            deleteNode(temp);
-            addNode(temp);
-            this->mp[key_] = this->head->next;
-            return res;
+    int get(int key) {
+        if (map.find(key) == map.end()) {
+            return -1;
         }
-        return -1;
-    }
+        Node *curr = map[key];
+        pushToEnd (curr);
 
-    void prinDLL(){
-        node* temp = this->head->next;
-        while(temp!=this->tail){
-            cout<<temp->key<<" ";
-            temp = temp->next;
-        }
-        cout<<endl;
+        return curr->val;
     }
     
     void put(int key, int value) {
-        if(this->mp.find(key)!=this->mp.end()){
-            node* temp = this->mp[key];
-            this->mp.erase(key);
-            deleteNode(temp);
+        if (map.find(key) != map.end()) {
+            Node *curr = map[key];
+            curr->val = value;
+            pushToEnd(curr);
         }
-
-        if(mp.size()==this->capacity){
-            // cout<<"Put (Before) :: "<<this->mp.size()<<"\n";
-            // prinDLL();
-            this->mp.erase(this->tail->prev->key);
-            deleteNode(this->tail->prev);
-            // cout<<"Put (After) :: "<<this->mp.size()<<"\n";
-
+        else {
+            Node *newnode = new Node (key, value);
+            if (currSize == cap) {
+                deleteLRUnode();
+            }
+            addToEnd(newnode);
         }
-
-        addNode(new node(key,value));
-        this->mp[key] = this->head->next;
     }
 };
