f8e535b15d5946971b6a938138f0817ccfb72f88
Pranav Gajera <pranavgajera1998@gmail.com>
2023-11-19 00:09:35 +0530

    Time: 2 ms (47.02%) | Memory: 6.5 MB (27.86%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    Solution(){
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
+    string defangIPaddr(string address) {
+        string result="";
+        for(char c : address){
+            if(c=='.') result+="[.]";
+            else result+=c;
+        }
+        return result;
+    }
+};
