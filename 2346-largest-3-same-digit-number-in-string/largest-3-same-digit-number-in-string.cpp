afb5d694b01050788925d62c54470846c14003e7
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-04 11:52:50 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (91.88%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        unordered_map<int,string> mp = {{-1,""},
-                                        {0,"000"},
-                                        {1,"111"},
-                                        {2,"222"},
-                                        {3,"333"},
-                                        {4,"444"},
-                                        {5,"555"},
-                                        {6,"666"},
-                                        {7,"777"},
-                                        {8,"888"},
-                                        {9,"999"}};
-        for(int i=9;i>=-1;i--)
-            if (num.find(mp[i]) != string::npos) return mp[i];
-        
-
-        return mp[-1];
+        char c1 = num[0],c2 = num[1];
+        int i=2;
+        int maxi = INT_MIN;
+        while(i<num.size()){
+            char c3 = num[i];
+            if(c1==c2 and c1==c3){
+                int sum = (int)(c1-'0') *100 + (int)(c2-'0') *10 + (int)(c3-'0');
+                maxi = max(maxi,sum); 
+            }
+            c1 = c2;
+            c2 = c3;
+            i++;
+        }
+        if(maxi==0) return "000";
+        return maxi == INT_MIN?"":to_string(maxi);
     }
 };
