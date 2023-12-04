75a1a954a723c692b42cf6c10dccf05df2460df1
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-04 15:49:45 +0530

    Time: 3 ms (95.38%) | Memory: 10.5 MB (64.77%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -1,52 +1,15 @@
 class Solution {
 public:
-    int local_cost(int x1,int y1,int x2,int y2){
-        if(x2>=x1 && y2>=y1){
-            if(x1==x2) return (y2-y1);
-            else if(y1==y2) return (x2-x1);
-            else{
-                float slop = (y2-y1)/((x2-x1)*1.0);
-                if(slop<1){
-                    return (x2-x1);
-                }else{
-                    return (y2-y1);
-                }
-            } 
-        }else if(x2>=x1 && y2<y1){
-            if(x1==x2) return (y1-y2);
-            else{
-                float slop = (y2-y1)/((x2-x1)*1.0);
-                if(slop<-1){
-                    return (y1-y2);
-                }else{
-                    return (x2-x1);
-                }
-            }
-        }else if(x2<x1 && y2>=y1){
-            if(y1==y2) return (x1-x2);
-            else{
-                float slop = (y2-y1)/((x2-x1)*1.0);
-                if(slop<-1){
-                    return (y2-y1);
-                }else{
-                    return (x1-x2);
-                }
-            }
-        }else if(x2<x1 && y2<y1){
-            float slop = (y2-y1)/((x2-x1)*1.0);
-            if(slop<1){
-                return (x1-x2);
-            }else{
-                return (y1-y2);
-            }
-        }
-        return 0;
-    }
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
         int time = 0;
-        for(int i=1;i<points.size();i++){
-            time += local_cost(points[i][0],points[i][1],points[i-1][0],points[i-1][1]);
-        }
+        for(int i=1;i<n;i++){
+            int x1 = points[i-1][0];
+            int y1 = points[i-1][1];
+            int x2 = points[i][0];
+            int y2 = points[i][1];
+            time += max(abs(y2-y1),abs(x2-x1));
+        }   
         return time;
     }
 };
