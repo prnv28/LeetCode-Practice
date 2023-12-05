ac65272f6979fc7f512e6ac2f690d5c3d201196a
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-05 15:36:48 +0530

    Time: 3 ms (50.35%) | Memory: 6.6 MB (84.44%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        if(t.length()==0 && s.length()==0) return true;
         int i=0;
         for(int j=0;j<t.size();j++){
             if(s[i]==t[j]) i++;
+            if(i==s.length())   return true;
         }
 
-        return i==s.size()?true:false;
+        return false;
     }
 };
