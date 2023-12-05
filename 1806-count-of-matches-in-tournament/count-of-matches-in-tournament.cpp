9fd9148b9d5c9115e6dff4ca6a9fb99e40320049
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-05 15:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (98.05%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int count = 0;
-        while(n>1){
-            int temp = (n>>1);
-            count += temp;
-            if(n&1){
-                n = temp+1;
-            }else{
-                n = temp;
-            }
-        }
-        return count;
+        return (n==1)?0:(n-1);
     }
 };
