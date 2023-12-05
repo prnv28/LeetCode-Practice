016537b983286672e7e2167088a4df4544b2416b
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-05 15:01:10 +0530

    Time: 2 ms (44.05%) | Memory: 6.4 MB (36.15%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -4,11 +4,11 @@ public:
         int count = 0;
         while(n>1){
             if(n&1){
-                count += (n-1)/2;
-                n = ((n-1)/2) +1;
+                count += ((n-1)>>1);
+                n =  (n>>1) + 1;
             }else{
-                count+= n/2;
-                n = n/2;
+                count += (n>>1);
+                n = n>>1;
             }
         }
         return count;
