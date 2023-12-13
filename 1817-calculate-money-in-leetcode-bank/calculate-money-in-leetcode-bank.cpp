c7a158b34ac0be4c3cc1fa3179db1df4a6607265
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-13 13:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (34.30%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -6,12 +6,8 @@ public:
         int total = 0;
         for(int i=7;i<7+weeks;i++){
             total += (i*(i+1))/2;
+            total -= ((i-7)*(i-6))/2;
         }
-        // cout<<total<<endl;
-        for(int j=0;j<weeks;j++){
-            total -= (j*(j+1))/2;
-        }
-        // cout<<total<<endl;
         for(int i=weeks+1;i<=weeks+rem;i++){
             total+= i;
         }
