2af4e41e9cd68050c220b3adf337bd23e4c5c7a3
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-18 12:02:10 +0530

    Time: 19 ms (83.95%) | Memory: 20.6 MB (97.27%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums.size();
         int smallest = INT_MAX,secondSmallest = INT_MAX;
         int biggest = 0,secondBiggest = 0;
+        
         for(int i=0;i<n;i++){
             if(nums[i]>=biggest){
                 secondBiggest = biggest;
