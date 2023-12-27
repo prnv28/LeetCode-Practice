4c883f132cc0fd283fe124429aa009914a5e64ca
Pranav Gajera <pranavgajera1998@gmail.com>
2023-12-27 21:05:39 +0530

    Time: 8 ms (64.88%) | Memory: 16 MB (58.45%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int i=0,j=numbers.size()-1;
         while(i<j){
             while(numbers[i]+numbers[j]>target) j--;
