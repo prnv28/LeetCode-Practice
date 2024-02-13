f6b62645b9f08c5874a5372861032ee67c4709bb
Pranav Gajera <pranavgajera1998@gmail.com>
2024-02-13 17:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (41.52%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int algo(vector<int>& nums){
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int first = nums[0];
+        int second = max(nums[0],nums[1]);
+        for(int i =2;i<n;i++){
+            int temp = second;
+            second = max(nums[i]+first,second);
+            first = temp;
+        }
+        return second;
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[1],nums[0]); 
+       vector<int> arr1;
+       vector<int> arr2;
+       arr1.push_back(nums[0]);
+       for(int i=1;i<n-1;i++){
+           arr1.push_back(nums[i]);
+           arr2.push_back(nums[i]);
+       }
+       arr2.push_back(nums[n-1]);
+       return max(algo(arr1),algo(arr2));
+    }
+};
