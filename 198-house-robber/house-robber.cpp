2dddddec0590e576351d8cf37fc7e400362eb170
Pranav Gajera <pranavgajera1998@gmail.com>
2024-02-13 16:03:01 +0530

    Time: 4 ms (30.65%) | Memory: 9.1 MB (39.24%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int n = nums.size();
+        
         if(n==0) return 0;
         if(n==1) return nums[0];
-        vector<int> dp(n,-1);
+
         int first = nums[0];
         int second = max(nums[0],nums[1]);
+        
         for(int i=2;i<n;i++){
             int temp = second;
             second = max(nums[i]+first,second);
             first = temp;
         } 
+        
         return second;
     }
 };
