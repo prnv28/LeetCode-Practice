284de39bcdf84d37aaeaf36af9ffa4d1a4174a63
Pranav Gajera <pranavgajera1998@gmail.com>
2024-03-04 16:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (49.63%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<int> curr(n);
         vector<int> prev(n);
         for(int i=0;i<m;i++){
