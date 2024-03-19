20f0b08574a19ba7e9ad1dd5a8cc4779fca4b81e
Pranav Gajera <pranavgajera1998@gmail.com>
2024-03-19 13:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (68.00%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -17,11 +17,15 @@ public:
         cout<<endl;
     }
     int minimumTotal(vector<vector<int>>& triangle) {
+
         if(triangle.empty()) return 0;
         int m = triangle.size();
         vector<int> prev(m,0);
         vector<int> curr(m,0);
+
         prev[0] = triangle[0][0];
+        curr[0] = triangle[0][0];
+        
         for(int i=0;i<triangle.size();i++){
             for(int j=0;j<=i;j++){
                 if(i==0 && j==0) curr[j] = triangle[i][j];
@@ -29,11 +33,8 @@ public:
                 else if(j<i) curr[j] = triangle[i][j] + min(prev[j],prev[j-1]); 
                 else curr[j] =  triangle[i][j] + prev[j-1];
             }
-            // pv(curr);
             prev = curr;
         }
-        // pv(prev);
-        
         return *min_element(prev.begin(),prev.end());
     }
 };
