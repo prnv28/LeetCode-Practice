c444a8e44bc09e598539402f1cfe6e4d8bf4f052
Pranav Gajera <pranavgajera1998@gmail.com>
2024-03-20 21:28:05 +0530

    Time: 4 ms (97.21%) | Memory: 13.1 MB (52.34%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        if(matrix.size()==0) return 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> prev(n,0);
+        vector<int> curr(n,0);
+        int mini = INT_MAX;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0){
+                    curr[j] = matrix[i][j];
+                }else if(j==0){
+                    curr[j] = matrix[i][j] + min(prev[j],prev[j+1]);
+                }else if(j==n-1){
+                    curr[j] = matrix[i][j] + min(prev[j-1],prev[j]);
+                }else{
+                    curr[j] = matrix[i][j] + min(min(prev[j-1],prev[j]),prev[j+1]);
+                }
+
+                if(i==m-1){
+                    mini = min(mini,curr[j]);
+                }
+            }
+            prev = curr;
+        }
+        return mini;
+    }
+};
