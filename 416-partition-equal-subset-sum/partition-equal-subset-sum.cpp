914b62e403ebc70798f8c163741674295968682a
Pranav Gajera <pranavgajera1998@gmail.com>
2024-04-12 14:42:41 +0530

    Time: 122 ms (72.05%) | Memory: 98.8 MB (18.54%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -18,7 +18,23 @@ public:
             total += nums[i];
         }
         if(total%2) return false;
-        vector<vector<int>> dp(n,vector<int> ((total/2)+1,-1));
-        return algo(n-1,total/2,nums,dp);
+        vector<vector<int>> dp(n,vector<int> ((total/2)+1,0));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=total/2;j++){
+                if(i==0){
+                    dp[i][j] = (j==nums[i]);
+                }else{
+                    int not_take = dp[i-1][j];
+                    int take = false;
+                    if(j>=nums[i]){
+                        take = dp[i-1][j-nums[i]];
+                    }
+                    dp[i][j] = take || not_take;
+                }
+            }
+        }
+        return dp[n-1][total/2];
+        // return algo(n-1,total/2,nums,dp);
     }
 };
