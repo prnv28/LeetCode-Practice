3d3f53200c13b2f4d4ca0ccf9e0e1db878693afa
Pranav Gajera <pranavgajera1998@gmail.com>
2024-07-02 10:16:38 +0530

    Time: 5 ms (37.19%) | Memory: 10.4 MB (83.57%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        int count = 0;
-        for(int i:arr){
-            if(i%2){
-                count++;
-            }else{
-                count = 0;
-            }
-            if(count==3){
-                return true;
-            }
+        if(arr.size()<3) return false;
+
+        for(int i =0;i<=arr.size()-3;i++){
+            if(arr[i]%2 && arr[i+1]%2 && arr[i+2]%2) return true;
         }
         return false;
     }
