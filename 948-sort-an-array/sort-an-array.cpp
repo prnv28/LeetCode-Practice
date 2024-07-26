47e68d5dc18931b0bfa3c15ca8c2662e57cadec7
Pranav Gajera <pranavgajera1998@gmail.com>
2024-07-26 11:34:23 +0530

    Time: 85 ms (89.54%) | Memory: 70.4 MB (69.18%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                int a =2;
+            }
+        }
         return nums;
     }
 };
