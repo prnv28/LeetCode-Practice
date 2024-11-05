0f49b8d5783563a6fe61c0087437e38004423e71
Pranav Gajera <pranavgajera1998@gmail.com>
2024-11-05 14:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (28.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        int i=0,j=s.size()-1;
-        while(i<j){
-            while(i < j && !iswalnum(tolower(s[i]))) i++;
-            while(i < j && !iswalnum(tolower(s[j]))) j--;
-            if(tolower(s[i])!=tolower(s[j])) return false;
-            i++;
-            j--;
+        string cs = "";
+        for(int i=0;i<s.size();i++){
+            int c = int(s[i]);
+            if(c>=65 && c<=90){
+                cs += char(c+32);
+            }else if((c>=48 && c<=57) || (c>=97 && c<=122)){
+                cs += char(c);
+            }
+        }
+        int n = cs.size();
+        if(n>0){
+            for(int i=0;i<=n/2;i++){
+                if(cs[i]!=cs[n-i-1]) return false;
+            }
         }
         return true;
     }
