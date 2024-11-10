50f240c9897b4dcf3a89c2507581365acafdd361
Pranav Gajera <pranavgajera1998@gmail.com>
2024-11-10 21:24:37 +0530

    Time: 3 ms (5.02%) | Memory: 8.4 MB (12.92%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> solution;
+        solution.push_back({1});
+        for(int i=1;i<numRows;i++){
+            vector<int> temp;
+            for(int j=0;j<=i;j++){
+                if(j==0){
+                    temp.push_back(1);
+                }else if(j>0 && j<i){
+                    temp.push_back(solution[i-1][j-1] + solution[i-1][j]);
+                }else{
+                    temp.push_back(1);
+                }
+            }
+            solution.push_back(temp);
+        }
+        return solution;
+    }
+};
