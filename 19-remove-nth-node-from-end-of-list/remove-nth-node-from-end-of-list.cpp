a488f103232b130ecdedfc49049a43567f28af86
Pranav Gajera <pranavgajera1998@gmail.com>
2024-12-16 17:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (7.14%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -17,11 +17,10 @@ public:
             if(n==0) return head;
             return NULL;
         }
+        
         ListNode* slow = head;
         ListNode* fast = head;
-        ListNode* tmp = NULL;
         while(n--){
-            tmp = fast;
             fast = fast->next;
         }
         
@@ -29,11 +28,11 @@ public:
             return head->next;
         }
 
-        while(fast!= NULL && fast->next!=NULL){
+        while(fast->next!=NULL){
             slow = slow->next;
             fast = fast->next;
         }
-
+        
         slow->next = slow->next->next;
         return head;
 
