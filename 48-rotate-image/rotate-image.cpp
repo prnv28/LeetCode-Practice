da23f7563251e7c770585c16a69a0b22310d8a64
Pranav Gajera <pranavgajera1998@gmail.com>
2024-12-18 15:16:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (21.75%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -18,17 +18,12 @@ public:
     }
 
     void vertical_mirror_image(vector<vector<int>>& matrix){
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size()/2;j++){
-                swap(matrix[i][j],matrix[i][matrix[i].size()-j-1]);
-            }
+        for(int j=0;j<matrix.size()/2;j++){
+            swap(matrix[j],matrix[matrix.size()-j-1]);
         }
     }
     void rotate(vector<vector<int>>& matrix) {
-        // printm(matrix);
-        transpose(matrix);
-        // printm(matrix);
         vertical_mirror_image(matrix);
-        // printm(matrix);
+        transpose(matrix);
     }
 };
