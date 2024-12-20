b3dff0635389789eddb693a0120b4d3ada3964d1
Pranav Gajera <pranavgajera1998@gmail.com>
2024-12-20 13:00:49 +0530

    Time: 16 ms (9.86%) | Memory: 15 MB (8.34%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,14 +9,31 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL) return false;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow==fast) return true;
+       // Initialize a pointer 'temp'
+    // at the head of the linked list
+    ListNode* temp = head;  
+    
+    // Create a map to keep track of
+    // encountered nodes
+    std::unordered_map<ListNode*, int> nodeMap;  
+
+    // Step 2: Traverse the linked list
+    while (temp != nullptr) {
+        // If the node is already in the
+        // map, there is a loop
+        if (nodeMap.find(temp) != nodeMap.end()) {
+            return true;
         }
-        return slow==fast;
+        // Store the current node
+        // in the map
+        nodeMap[temp] = 1;
+        
+        // Move to the next node
+        temp = temp->next;  
+    }
+
+    // Step 3: If the list is successfully traversed 
+    // without a loop, return false
+    return false;
     }
 };
