716d2562bf62aa46787ba1eca4c446541178b9c2
Pranav Gajera <pranavgajera1998@gmail.com>
2024-12-21 15:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (12.83%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int maxi = 0;
         int temp = 0;
         for(int i : nums){
