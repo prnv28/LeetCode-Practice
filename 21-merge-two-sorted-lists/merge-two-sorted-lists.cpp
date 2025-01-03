411d5425817278f762bb3285e9f7332fb500c81e
Pranav Gajera <pranavgajera1998@gmail.com>
2025-01-03 15:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (10.14%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -16,13 +16,26 @@ public:
         cout.tie(NULL);
     }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
-        if(list1->val<=list2->val){
-            list1->next = mergeTwoLists(list1->next,list2);
-            return list1;
-        }
-        list2->next = mergeTwoLists(list1,list2->next);
-        return list2;
+       ListNode* dummy = new ListNode(-1);
+       ListNode* head = dummy;
+
+       while(list1!=NULL && list2!=NULL){
+            if(list1->val<list2->val){
+                dummy->next = list1;
+                dummy = dummy->next;
+                list1 = list1->next;
+            }else{
+                dummy->next = list2;
+                dummy = dummy->next;
+                list2 = list2->next;
+            }
+       }
+       if(list1==NULL){
+            dummy->next = list2;
+       }
+       if(list2==NULL){
+            dummy->next = list1;
+       }
+       return head->next;
     }
 };
