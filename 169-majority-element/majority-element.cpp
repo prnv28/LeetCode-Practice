ef80ab7b3d2ffbc01510ab09c6844362bae31503
Pranav Gajera <pranavgajera1998@gmail.com>
2025-01-08 17:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (19.80%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,19 +4,22 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        int vote = 1;
-        int majority = nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==majority){
+        int vote = 0;
+        int majority;
+        for(int i=0;i<nums.size();i++){
+            if(vote<=0){
+                vote=1;
+                majority = nums[i];
+            }else if(nums[i]==majority){
                 vote++;
             }else{
                 vote--;
             }
-            if(vote<=0){
-                vote=1;
-                majority = nums[i];
-            }
         }
-        return majority;
+        int cnt = 0;
+        for(int num : nums){
+            if(num==majority) cnt++;
+        }
+        return cnt>=nums.size()/2?majority:-1;
     }
 };
