e98cd6d23a4bcf5e1bdd6ceca8779413750ede3f
Pranav Gajera <pranavgajera1998@gmail.com>
2025-01-08 17:00:27 +0530

    Time: 2 ms (37.24%) | Memory: 28.3 MB (19.80%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int vote = 1;
+        int majority = nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==majority){
+                vote++;
+            }else{
+                vote--;
+            }
+            if(vote<=0){
+                vote=1;
+                majority = nums[i];
+            }
+        }
+        return majority;
+    }
+};
