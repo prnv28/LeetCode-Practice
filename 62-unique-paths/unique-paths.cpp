4789a1a34338983078dec066ff21178a138d73c4
Pranav Gajera <pranavgajera1998@gmail.com>
2025-01-08 17:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (24.79%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
+    int algo(int i,int j,vector<vector<int>>& dp){
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==0 && j==0) return dp[i][j] = 1;
+        if(i<0) return 0;
+        if(j<0) return 0;
+        if(i==0) return dp[i][j] = algo(i,j-1,dp);
+        if(j==0) return dp[i][j] = algo(i-1,j,dp);
+        return dp[i][j] = algo(i,j-1,dp) + algo(i-1,j,dp);
+    }
     int uniquePaths(int m, int n) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        vector<int> curr(n);
-        vector<int> prev(n);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0){
-                    curr[j] = 1;
-                }else{
-                    curr[j] = prev[j] + curr[j-1];
-                }
-            }
-            prev = curr;
-        }
-        return curr[n-1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        cout<<dp[0][0]<<endl;
+        return algo(m-1,n-1,dp);
     }
 };
