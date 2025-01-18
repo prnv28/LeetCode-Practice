ec9e54d71a7fce26d20a5ea2748d9da25807a441
Pranav Gajera <pranavgajera1998@gmail.com>
2025-01-18 13:15:17 +0530

    Time: 1 ms (60.34%) | Memory: 16.4 MB (29.64%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,23 +11,41 @@
  */
 class Solution {
 public:
-    void algo(TreeNode* root,int level, int depth,map<int,map<int,multiset<int>>>& mp){
-        if(!root) return;
-        mp[level][depth].insert(root->val);
-        algo(root->left,level-1,depth+1,mp);
-        algo(root->right,level+1,depth+1,mp);
-    }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> result;
+
         map<int,map<int,multiset<int>>> mp;
-        algo(root,0,0,mp);
+
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+
+        while(!q.empty()){
+            int size = q.size();
+
+            while(size--){
+                TreeNode* node = q.front().first;
+                int level = q.front().second.first;
+                int depth = q.front().second.second;
+                mp[level][depth].insert(node->val);
+                q.pop();
+
+                if(node->left){
+                    q.push({node->left,{level-1,depth+1}});
+                }
+                if(node->right){
+                    q.push({node->right,{level+1,depth+1}});
+                }
+            }
+        }
+
         for(auto itr : mp){
             vector<int> col;
             for(auto it : itr.second){
                 col.insert(col.end(),it.second.begin(),it.second.end());
             }
             result.push_back(col);
+            col.clear();
         }
-        return result;
+         return result;
     }
 };
