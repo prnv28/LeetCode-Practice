5b1d4bebfe5334799043738dd3493f8b92258223
Pranav Gajera <pranavgajera1998@gmail.com>
2025-01-31 20:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (15.99%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        double ans = 1.0;
         long nn = n;
+        double ans = 1.0;
         if(nn<0) nn = -1*nn;
         while(nn>0){
             if(nn%2==1){
-                ans = ans*x;
-                nn = nn - 1;
+                ans *= x;
+                nn = nn -1;
             }else{
-                x = x * x;
-                nn = nn/2;
+                x = x*x;
+                nn/=2;
             }
         }
-        if(n<0) ans = double(1.0/ans);
+        if(n<0) ans  = double(1.0/ans);
         return ans;
     }
 };
