0748cc19273ed5ae9c4415ccc02c87f7d33bc10a
Pranav Gajera <pranavgajera1998@gmail.com>
2025-01-31 19:58:28 +0530

    Time: 39 ms (95.50%) | Memory: 28.9 MB (85.44%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
         vector<vector<int>> result;
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                int low=i+1;
-                int high = n-1;
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
                 int sum = 0-nums[i];
+                int low = i+1;
+                int high = n-1;
                 while(low<high){
-                    if(nums[low]+nums[high]==sum) {
+                    if(nums[low]+nums[high]==sum){
                         result.push_back({nums[i],nums[low],nums[high]});
-                        while(low<high && nums[low]==nums[low+1]) low++;
-                        while(low<high && nums[high]==nums[high-1]) high--;
+                        while(low<high && nums[low+1]==nums[low]) low++;
+                        while(high>low && nums[high-1]==nums[high]) high--;
                         low++;
                         high--;
-                    }else if(nums[low]+nums[high]>sum){
-                        high--;
+                    }else if(nums[low]+nums[high]<sum){
+                        low++;
                     }else{
-                        low++;
-                    } 
+                        high--;
+                    }
                 }
             }
         }
