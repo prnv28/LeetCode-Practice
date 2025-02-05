8eb367a900bd14f2ebb286b90ee6b539aba35876
Pranav Gajera <pranavgajera1998@gmail.com>
2025-02-05 20:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (76.04%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int first = 0;
+        int last = (m*n) - 1;
+        while(first<=last){
+            int mid = (first+last)/2;
+            int i = mid/n;
+            int j = mid%n;
+            // cout<<mid<<" : "<<i<<" : "<<j<<endl;
+            if(matrix[i][j]<target){
+                first = mid+1;
+            }else if(matrix[i][j]>target){
+                last = mid-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
