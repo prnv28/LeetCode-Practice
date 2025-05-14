3b0dc25d46087f33ee73e1939ec39a788c7d5db9
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-14 21:59:08 +0530

    Time: 4 ms (12.01%) | Memory: 71.9 MB (17.98%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_so_far = INT_MIN;
-        int maxi = 0;
-        for(int i =0;i<nums.size();i++){
-            maxi += nums[i];
-            max_so_far = max(maxi,max_so_far);
-            maxi = max(0,maxi);
-        }
-        return max_so_far;
+        int max_so_fat = INT_MIN;
+        int max_lat = 0;
+        for(int i=0;i<nums.size();i++){
+            max_lat += nums[i];
+            max_so_fat = max(max_so_fat,max_lat);
+            if(max_lat<0){
+                max_lat = 0;
+            }
+            
+        }   
+        return max_so_fat;
     }
 };
