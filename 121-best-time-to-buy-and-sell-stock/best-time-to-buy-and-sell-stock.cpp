630db7052ce4bd125913a2300eaa1c2ebffd08df
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-15 19:12:20 +0530

    Time: 3 ms (29.10%) | Memory: 97.2 MB (87.06%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int algo(int ind,int min_price,vector<int>& prices){
-        if(ind>=prices.size()) return 0;
-        return max(prices[ind]-min_price,algo(ind+1,min(min_price,prices[ind]),prices));
-    }
     int maxProfit(vector<int>& prices) {
-        // return algo(0,INT_MAX,prices);
-        std::ios::sync_with_stdio(0);
-        std::cin.tie(0);
-        int min_prices = INT_MAX;
+        int min_price = INT_MAX;
         int max_profit = 0;
         for(int i=0;i<prices.size();i++){
-            min_prices = min(min_prices,prices[i]);
-            max_profit = max(prices[i]-min_prices,max_profit);
+            min_price = min(min_price,prices[i]);
+            max_profit = max(max_profit,prices[i]-min_price);
         }
         return max_profit;
     }
