c92152a975608a1984234ed40c640e2f83edf62b
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-16 16:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (8.35%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -8,34 +8,40 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
 
-        int cnt1=0, cnt2=0;
-        int el1 = INT_MIN ,el2 = INT_MIN;
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
 
         for(int i=0;i<n;i++){
             if(cnt1==0 && el2!=nums[i]){
-                cnt1 = 1;
+                cnt1++;
                 el1 = nums[i];
-            }else if(cnt2==0 && nums[i]!=el1){
-                cnt2 = 1;
+            }else if(cnt2==0 && el1!=nums[i]){
+                cnt2++;
                 el2 = nums[i];
             }else if(nums[i]==el1){
                 cnt1++;
-            }else if(nums[i]==el2 && el1!=el2){
+            }else if(nums[i]==el2){
                 cnt2++;
             }else{
                 cnt1--;
                 cnt2--;
             }
         }
-        int c1 = 0, c2 = 0;
-        vector<int> result;
-        for(int n : nums){
-            if(n==el1) c1++;
-            if(n==el2) c2++;
-        }
-        if(c1>n/3) result.push_back(el1);
-        if(c2>n/3) result.push_back(el2);
 
-        return result;
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1) cnt1++;
+            if(nums[i]==el2) cnt2++;
+        }
+
+        vector<int> tmp;
+        if(cnt1>n/3){
+            tmp.push_back(el1);
+        }
+        if(cnt2>n/3){
+            tmp.push_back(el2);
+        }
+        return tmp;
     }
 };
