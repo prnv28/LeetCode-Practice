8ab6b14799290769defdadde41e5abefe8208bc7
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-24 22:15:43 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (9.02%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void algo(int i, vector<int>& nums, vector<int> ds, set<vector<int>>&res){
+        if(i==nums.size()){
+            sort(ds.begin(),ds.end());
+            res.insert(ds);
+            return;
+        }
+        ds.push_back(nums[i]);
+        algo(i+1,nums,ds,res);
+        ds.pop_back();
+        algo(i+1,nums,ds,res);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> res;
+        vector<int> ds;
+        algo(0,nums,ds,res);
+        for(auto it : res){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
