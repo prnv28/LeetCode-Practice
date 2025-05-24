26fecaf2c5f98ea4df1126c896e15fc6a54f4ebb
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-24 23:21:17 +0530

    Time: 2 ms (41.48%) | Memory: 10 MB (44.56%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void algo(int i, vector<int>& nums, vector<int> ds, set<vector<int>>&res){
-        if(i==nums.size()){
-            sort(ds.begin(),ds.end());
-            res.insert(ds);
-            return;
+    void algo(int i, vector<int>& nums, vector<int> &ds,vector<vector<int>>& ans){
+        ans.push_back(ds);
+        for(int ind = i;ind<nums.size();ind++){
+            if(ind!=i && nums[ind]==nums[ind-1]) continue;
+            ds.push_back(nums[ind]);
+            algo(ind+1,nums,ds,ans);
+            ds.pop_back();
         }
-        ds.push_back(nums[i]);
-        algo(i+1,nums,ds,res);
-        ds.pop_back();
-        algo(i+1,nums,ds,res);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
         vector<vector<int>> ans;
-        set<vector<int>> res;
         vector<int> ds;
-        algo(0,nums,ds,res);
-        for(auto it : res){
-            ans.push_back(it);
-        }
+        sort(nums.begin(),nums.end());
+        algo(0,nums,ds,ans);
         return ans;
     }
 };
