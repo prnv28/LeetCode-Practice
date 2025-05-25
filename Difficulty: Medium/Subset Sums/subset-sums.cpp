5d553d94a9820ad64d72685d431d6f47b9174a98
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-25 13:38:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.cpp
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -1,61 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    void algo(int i,vector<int>& arr, vector<int>& result,int sum){
-        if(i<0){
-            result.push_back(sum);
-            return;
+    
+    void algo(int i,vector<int>& arr, int sum, vector<int>& ans){
+        if(i==arr.size()){
+            ans.push_back(sum);
+        } else{
+            algo(i+1, arr, sum+arr[i], ans);
+            algo(i+1,arr,sum,ans);
         }
-        algo(i-1,arr,result,arr[i]+sum);
-        algo(i-1,arr,result,sum);
+        return;
     }
     vector<int> subsetSums(vector<int>& arr) {
-        vector<int> result;
-        int n = arr.size();
-        algo(n-1,arr,result,0);
-        return result;
+        vector<int> ans;
+        algo(0,arr,0,ans);
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t; // Number of test cases
-    cin >> t;
-    cin.ignore(); // Ignore the newline after the integer input
-
-    while (t--) {
-        vector<int> inputArray;
-        string inputLine;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, inputLine);
-        stringstream inputStream(inputLine);
-        int num;
-        while (inputStream >> num) {
-            inputArray.push_back(num); // Read the array elements from input string
-        }
-
-        Solution solutionObject;
-        vector<int> result = solutionObject.subsetSums(inputArray);
-        sort(result.begin(), result.end());
-
-        for (int i = 0; i < result.size(); i++) {
-            if (i != 0)
-                cout << " ";
-            cout << result[i];
-        }
-        cout
-            << endl
-            << "~\n"; // Print results in list format with new line after each test case
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
