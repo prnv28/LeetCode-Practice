06fc22a2fdc7691450c092541a093c6c9fd8a43e
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-26 15:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (24.01%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    void algo(int i,int target,vector<int>& nums, vector<int>&ds, vector<vector<int>>& ans){
-        if(target==0){
-            ans.push_back(ds);
-            return;
-        }
-        for(int ind = i;ind<nums.size();ind++){
-            if(i!=ind && nums[ind]==nums[ind-1]) continue;
-            if(target>=nums[ind]){
-                ds.push_back(nums[ind]);
-                algo(ind+1,target-nums[ind],nums,ds,ans);
-                ds.pop_back();
-            }else{
-                break;
-            }
-            
-        }
-    }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        vector<vector<int>> ans;
-        vector<int> ds;
-        sort(nums.begin(),nums.end());
-        algo(0,target,nums,ds,ans);
-        return ans;    
-    }
+    
+    void sumF(int ind,vector<int> &arr,set<vector<int>> &s, vector<int> &ds, vector<vector<int>> &result, int target){
 
+        if(target==0 && s.find(ds)==s.end()){
+            s.emplace(ds);
+            result.push_back(ds);
+        }
+        if(ind == arr.size()) return;
+        
+        
+        for(int i=ind;i<arr.size();i++){
+            
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+            if (arr[i] > target) break; 
+            ds.push_back(arr[i]);
+            sumF(i+1,arr,s,ds,result,target-arr[i]);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> ds;
+        set<vector<int>> s;
+        sort(candidates.begin(),candidates.end());
+        sumF(0,candidates,s,ds,result,target);
+        return result;
+    }
 };
