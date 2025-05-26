8722f6d005a1b2a238f8fb51d5de3541a8744b75
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-26 15:47:10 +0530

    Time: 29 ms (92.20%) | Memory: 53.3 MB (98.78%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPallindrom(string& s, int start, int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void partitionHelper(int index, string& s, vector<string>& ds, vector<vector<string>>& result){
+        if(s.size()==index){
+            result.push_back(ds);
+            return;
+        }
+        for(int i=index;i<s.size();i++){
+            if(isPallindrom(s,index,i)){
+                ds.push_back(s.substr(index, i - index + 1));
+                partitionHelper(i+1,s,ds,result);
+                ds.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> ds;
+        partitionHelper(0,s,ds,result);
+        return result;
+    }
+};
