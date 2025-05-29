6d02ffed6e6bcdfc4dc62ce6fddbe53419f1ec50
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-29 09:25:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (30.39%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -10,42 +10,22 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head){
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        while(curr!=NULL){
-            ListNode* nxt = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
-        }
-        return prev;
-    }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head) return head;
-        ListNode* temp = head;
-        int len = 0;
-        while(temp!=NULL){
-            len++;
-            temp = temp->next;
+        if(head==NULL || head->next==NULL || k==0) return head;
+        ListNode* tmp = head;
+        int length = 1;
+        while(tmp->next!=NULL){
+            length++;
+            tmp = tmp->next;
         }
-        k = k%len;
-        if(k==0) return head;
-        head = reverse(head);
-        temp = head;
-        ListNode* prev = NULL;
-        while(k--){
-            prev = temp;
-            temp = temp->next;
+        tmp->next = head;
+        k = k%length;
+        int end = length - k;
+        while(end--){
+            tmp = tmp->next;
         }
-        prev->next = NULL;
-        temp = reverse(temp);
-        head = reverse(head);
-        prev = head;
-        while(prev->next!=NULL){
-            prev = prev->next;
-        }
-        prev->next = temp;
+        head = tmp->next;
+        tmp->next = NULL;
         return head;
     }
 };
