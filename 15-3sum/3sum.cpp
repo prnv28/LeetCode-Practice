f5a740737702b91b3151f7c77b9859311291a6d9
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-29 14:41:36 +0530

    Time: 53 ms (44.37%) | Memory: 29 MB (90.27%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
+    void printVoV(vector<int>& V){
+        for(int v : V){
+            cout<<v<<" ";
+        }
+        cout<<endl;
+    }
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                int sum = 0-nums[i];
-                int low = i+1;
-                int high = n-1;
-                while(low<high){
-                    if(nums[low]+nums[high]==sum){
-                        result.push_back({nums[i],nums[low],nums[high]});
-                        while(low<high && nums[low+1]==nums[low]) low++;
-                        while(high>low && nums[high-1]==nums[high]) high--;
-                        low++;
-                        high--;
-                    }else if(nums[low]+nums[high]<sum){
-                        low++;
-                    }else{
-                        high--;
-                    }
+        // printVoV(nums);
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            
+            if(i!=0 && nums[i]==nums[i-1]){
+                // cout<<"Start!"<<endl;
+                // cout<<i<<" "<<nums[i]<<" "<<nums[i-1]<<endl;
+                // cout<<"end!"<<endl;
+                continue;
+            }
+            // while(i!=0 && nums[i]==nums[i-1]) continue;
+            // while(true) {
+            //     continue;
+            // }
+            int low = i+1;
+            int high = n-1;
+            
+            while(low<high){
+                int sum = nums[i]+nums[low]+nums[high];
+                // cout<<i<<" "<<low<<" "<<high<<" "<<sum<<endl;
+                if(sum<0){
+                    low++;
+                }else if(sum>0){
+                    high--;
+                }else{
+                    ans.push_back({nums[i],nums[low],nums[high]});
+                    low++;
+                    high--;
+                    while(low<high && nums[low]==nums[low-1]) low++;
+                    while(low<high && nums[high]==nums[high+1]) high--;
                 }
             }
         }
-        return result;
+        return ans;
     }
 };
