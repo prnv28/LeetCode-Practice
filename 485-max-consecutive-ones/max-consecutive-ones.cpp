20253880b3f774e10815648592d38345f35bfd08
Pranav Gajera <pranavgajera1998@gmail.com>
2025-05-29 19:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (64.80%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,10 +6,11 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
                 curr_max++;
+                max_so_far = max(max_so_far,curr_max);
             }else{
                 curr_max = 0;
             }
-            max_so_far = max(max_so_far,curr_max);
+            
         }
         return max_so_far;
     }
