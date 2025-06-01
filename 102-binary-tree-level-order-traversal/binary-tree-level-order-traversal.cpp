e721a274096c72ef5b1333a4d3810d7670182784
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-01 22:43:54 +0530

    Time: 3 ms (28.13%) | Memory: 17 MB (88.12%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -11,6 +11,11 @@
  */
 class Solution {
 public:
+    Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
     vector<vector<int>> levelOrder(TreeNode* root) {
         if(!root) return {};
         vector<vector<int>> ans;
