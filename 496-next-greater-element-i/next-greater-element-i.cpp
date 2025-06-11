8cfa99b0338e2da709ec4b82ff2cee8ecfa389fd
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-11 12:48:56 +0530

    Time: 3 ms (43.90%) | Memory: 12.7 MB (58.97%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> mp;
+    vector<int> nextLargerElement(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1) return {-1};
+        vector<int> result(n,-1);
         stack<int> s;
-        int n = nums2.size();
-        for(int i=n-1;i>=0;i--){
-            if(!s.empty() && s.top()>nums2[i]){
-                mp[nums2[i]] = s.top();
-            }else{
-                while(!s.empty() && s.top()<=nums2[i]){
+        s.push(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(!s.empty() && s.top()>arr[i]){
+                result[i]=s.top();
+            }else if(!s.empty() && s.top()<=arr[i]){
+                while(!s.empty() && s.top()<=arr[i]){
                     s.pop();
                 }
-                mp[nums2[i]] = s.empty()?-1:s.top();
+                if(!s.empty()){
+                    result[i] = s.top();
+                }
             }
-            s.push(nums2[i]);
-        }
-       
-        n = nums1.size();
-        vector<int> result(n,-1);
-        for(int i=0;i<n;i++){
-            result[i] = mp[nums1[i]];
+            s.push(arr[i]);
+            
         }
         return result;
+    }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result2 = nextLargerElement(nums2);
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<nums2.size();i++){
+            mp[nums2[i]] = i;
+        }
+        int n = nums1.size();
+        vector<int> result(n);
+        for(int i=0;i<nums1.size();i++){
+            int index = mp[nums1[i]];
+            result[i] = result2[index];
+        }
         
+        return result;
     }
 };
