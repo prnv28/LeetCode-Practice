d8253434b72d2c788c3662773afdcc2ec64b42b6
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-13 11:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (63.83%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -18,14 +18,38 @@ public:
         }
         return true;
     }
+
+    bool DFS(int src,int clr,vector<int>& color,vector<vector<int>>& list){
+        color[src] = clr;
+        for(int ch : list[src]){
+            if(color[ch]==-1){
+                if(!DFS(ch,!clr,color,list)){
+                    return false;
+                }
+            }
+            else if(color[ch]==color[src]){
+                return false;
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& list) {
+        string mode = "BFS";
+        mode = "DFS";
         int V = list.size();
         vector<int> color(V,-1);
         for(int i=0;i<V;i++){
             if(color[i]==-1){
-                if(!BFS(i,color,list)){
-                    return false;
+                if(mode=="BFS"){
+                    if(!BFS(i,color,list)){
+                        return false;
+                    }
+                }else{
+                    if(!DFS(i,0,color,list)){
+                        return false;
+                    }
                 }
+                
             }
         }
         return true;
