6221f054b25279d14da6f003ab69ee61e2f618b7
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-17 23:07:05 +0530

    Time: 407 ms (83.23%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,5 @@
 SELECT B.id 
 FROM Weather as A JOIN Weather as B
-WHERE DATEDIFF(B.recordDate,A.recordDate)=1 AND a.temperature<B.temperature;
+ON DATE_ADD(A.recordDate, INTERVAL 1 DAY) = DATE(B.recordDate)
+WHERE A.temperature<B.temperature;
+-- WHERE DATEDIFF(B.recordDate,A.recordDate)=1 AND a.temperature<B.temperature;
