32e598eb71af5e4d605d7da5d377bc101d06ca8b
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-17 23:43:09 +0530

    Time: 262 ms (70.30%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,8 +1,9 @@
-SELECT A.machine_id, ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+/* Write your T-SQL query statement below */
+SELECT a.machine_id, ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
 FROM Activity a, 
      Activity b
 WHERE a.machine_id = b.machine_id
 AND a.process_id = b.process_id
 AND a.activity_type = 'start'
 AND b.activity_type = 'end'
-GROUP BY machine_id
+GROUP BY a.machine_id
