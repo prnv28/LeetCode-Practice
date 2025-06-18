830c9f841c81f98b211e4b7571a44a7ff48578d9
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-18 11:03:12 +0530

    Time: 926 ms (61.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,11 @@
+SELECT
+    S.student_id
+    ,S.student_name
+    ,SU.subject_name
+    ,COUNT(E.student_id) attended_exams
+FROM Students S
+CROSS JOIN Subjects SU LEFT JOIN Examinations E
+    ON E.student_id = S.student_id
+    AND E.subject_name = SU.subject_name
+GROUP BY S.student_id,SU.subject_name
+ORDER BY S.student_id, SU.subject_name
