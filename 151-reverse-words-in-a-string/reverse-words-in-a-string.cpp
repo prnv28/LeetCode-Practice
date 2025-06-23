b9cc94788002c22658f1a591e741b5c3766281d7
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-23 15:18:18 +0530

    Time: 3 ms (43.13%) | Memory: 11.1 MB (35.71%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void print(vector<string>& ds){
+        for(auto s : ds){
+            cout<<s<<" ";
+        }
+        cout<<endl;
+    }
+    string reverseWords(string s) {
+        vector<string> ds;
+        string tmp = "";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                tmp = tmp + s[i];
+                if(i==s.size()-1){
+                    ds.push_back(tmp);
+                }
+            }else if(tmp.size()>0 && s[i]==' '){
+                ds.push_back(tmp);
+                tmp = "";
+            }
+        }
+        reverse(ds.begin(),ds.end());
+        string ans = "";
+        for(int i=0;i<ds.size();i++){
+            ans += ds[i];
+            if(i==ds.size()-1){
+                return ans;
+            }
+            ans += " ";
+        }
+        return ans;
+    }
+};
