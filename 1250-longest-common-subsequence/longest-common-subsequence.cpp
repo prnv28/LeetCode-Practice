35044a2217acb8760cee00fef957d9bca0fdd30d
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-23 13:09:08 +0530

    Time: 25 ms (67.40%) | Memory: 27.3 MB (76.97%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    void printVoV(vector<vector<int>> V){
-        for(auto v : V){
-            for(int i : v){
-                cout<<i<<" ";
-            }
-            cout<<endl;
-        }
-    }
     int longestCommonSubsequence(string text1, string text2) {
-        int len1 = text1.size();
-        int len2 = text2.size();
-        vector<int> cur(len2 + 1 , 0), prev(len2+1,0);
-        // string s = "";
-        for(int i=0;i<len1;i++){
-            for(int j=0;j<len2;j++){
-                if(text1[i]==text2[j]){
-                    cur[j+1] = 1 + prev[j]; 
-                    // s+=text1[i];
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }else{
-                    cur[j+1] = max(prev[j+1],cur[j]);
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            prev = cur;
         }
-        return cur[len2];
+        return dp[n][m];
     }
 };
