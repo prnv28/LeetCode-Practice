de72bcc2e90e37565e7575655769b7b3611b4861
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-23 12:48:12 +0530

    Time: 672 ms (5.00%) | Memory: 296.6 MB (12.13%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int algo(int ind,int prev_ind,vector<int>& nums,vector<vector<int>>& dp){
+        if(ind==nums.size()){
+            return 0;
+        }
+        if(dp[ind][prev_ind+1]!=-1){
+            return dp[ind][prev_ind+1];
+        }
+        if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
+            return dp[ind][prev_ind+1] = max(1 + algo(ind+1,ind,nums,dp),algo(ind+1,prev_ind,nums,dp));
+        }else{
+            return dp[ind][prev_ind+1] = algo(ind+1,prev_ind,nums,dp);
+        }
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+        algo(0,-1,nums,dp);
+        return dp[0][0];
+    }
+};
