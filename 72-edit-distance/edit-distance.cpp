8e454e0ded359afef17dd171c3cca0b0207e6776
Pranav Gajera <pranavgajera1998@gmail.com>
2025-06-26 18:27:51 +0530

    Time: 5 ms (71.29%) | Memory: 13.7 MB (23.62%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -15,23 +15,28 @@ public:
         int m = word2.size();
         vector<vector<int>> dp(n+1,vector<int> (m+1,0));
 
-        for(int i=0;i<=n;i++){
-            dp[i][0] = i;
-        }
+        vector<int> curr(m+1,0);
+        vector<int> prev(m+1,0);
+
         for(int j=0;j<=m;j++){
-            dp[0][j] = j;
+            prev[j] = j;
         }
 
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
+            for(int j=0;j<=m;j++){
+                if(j==0){
+                    curr[j]=i;
                 }else{
-                    dp[i][j] = 1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                    if(word1[i-1]==word2[j-1]){
+                        curr[j] = prev[j-1];
+                    }else{
+                        curr[j] = 1 + min(prev[j],min(curr[j-1],prev[j-1]));
+                    }
                 }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
         
     }
 };
