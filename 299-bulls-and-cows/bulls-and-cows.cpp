4e3a5370c85b9be7631eb605c28b4da92d72b00a
Pranav Gajera <pranavgajera1998@gmail.com>
2025-08-02 12:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (81.64%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls=0;
+        int n = secret.size();
+        map<char,pair<int,int>> mp;
+        for(int i=0;i<n;i++){
+            if(secret[i]==guess[i]){
+                bulls++;
+            }else{
+                if(mp.find(secret[i])==mp.end()){
+                    mp[secret[i]].first=1;
+                    mp[secret[i]].second=0;
+                }else{
+                    mp[secret[i]].first++;
+                }
+                if(mp.find(guess[i])==mp.end()){
+                    mp[guess[i]].first=0;
+                    mp[guess[i]].second=1;
+                }else{
+                    mp[guess[i]].second++;
+                }
+            }
+        }
+        int nonbulls = 0;
+        for(auto it : mp){
+            // cout<<it.first<<" ("<<it.second.first<<","<<it.second.second<<")"<<endl;
+            if(it.second.first>0 && it.second.second>0){
+                nonbulls+=min(it.second.first,it.second.second);
+            }
+        }
+        return to_string(bulls)+"A"+to_string(nonbulls)+"B";
+    }
+};
