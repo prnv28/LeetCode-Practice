7c3fd25d536e4fa41171de88b7fcd75365da36c5
Pranav Gajera <pranavgajera1998@gmail.com>
2025-08-02 11:04:59 +0530

    Time: 81 ms (37.35%) | Memory: 89.6 MB (64.04%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -11,23 +11,20 @@ public:
         if(n==0) return {};
         if(n==1) return {0};
         vector<int> prefix(n,0);
-        vector<int> suffix(n,0);
         vector<int> ans(n,0);
         prefix[0] = nums[0];
-        suffix[n-1] = nums[n-1];
+        int total = nums[0];
         for(int i=1;i<n;i++){
+            total += nums[i];
             prefix[i] = prefix[i-1]+nums[i];
-            suffix[n-i-1] = suffix[n-i] + nums[n-i-1];
         }
-        // printV(prefix);
-        // printV(suffix);
         for(int i=0;i<n;i++){
             if(i==0){
-                ans[i] = (suffix[i+1] - ((n-1-i)*nums[i]));
+                ans[i] = (total - prefix[i] - ((n-1-i)*nums[i]));
             }else if(i==n-1){
                 ans[i] = (((i)*nums[i]) - prefix[i-1]);
             }else{
-                ans[i] = (((i)*nums[i]) - prefix[i-1]) + (suffix[i+1] - ((n-i-1)*nums[i]));
+                ans[i] = (((i)*nums[i]) - prefix[i-1]) + (total - prefix[i] - ((n-1-i)*nums[i]));
             }
             
         }
