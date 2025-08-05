37cea4046beeb6a3ee341c7972ac908d75de4c8c
Pranav Gajera <pranavgajera1998@gmail.com>
2025-08-05 17:22:42 +0530

    Time: 6 ms (48.59%) | Memory: 23.8 MB (80.16%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,13 +5,8 @@ public:
         sort(intervals.begin(),intervals.end());
         result.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++){
-            vector<int> tmp = result.back();
-            int tmps = tmp[0];
-            int tmpe = tmp[1];
-            int news = intervals[i][0];
-            int newe = intervals[i][1];
-            if(news<=tmpe){
-                result.back()[1] = max(tmpe,newe);
+            if(intervals[i][0]<=result.back()[1]){
+                result.back()[1] = max(result.back()[1],intervals[i][1]);
             }else{
                 result.push_back(intervals[i]);
             }
