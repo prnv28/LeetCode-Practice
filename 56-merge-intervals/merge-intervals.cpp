aa407c8086bed9c6990bd4fcbe5ce7361ae5e041
Pranav Gajera <pranavgajera1998@gmail.com>
2025-08-05 17:21:31 +0530

    Time: 10 ms (16.57%) | Memory: 24.8 MB (16.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    void print(vector<vector<int>>& intervals){
-        for(auto intvl : intervals){
-            cout<<"["<<intvl[0]<<","<<intvl[1]<<"], ";
-        }
-        cout<<endl;
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        // sort(intervals.begin(),intervals.end());
-        // vector<vector<int>> result;
-        // result.push_back(intervals[0]);
-        // for(int i=1;i<intervals.size();i++){
-        //     if(result.back()[1]>=intervals[i][0]){
-        //         result.back()[1] = max(result.back()[1],intervals[i][1]);
-        //     }else{
-        //         result.push_back(intervals[i]);
-        //     }
-        // }
-        // return result;
         vector<vector<int>> result;
         sort(intervals.begin(),intervals.end());
         result.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++){
-            if(result.back()[1]>=intervals[i][0]){
-                vector<int> tmp = {result.back()[0],max(result.back()[1],intervals[i][1])};
-                result.pop_back();
-                result.push_back(tmp);
+            vector<int> tmp = result.back();
+            int tmps = tmp[0];
+            int tmpe = tmp[1];
+            int news = intervals[i][0];
+            int newe = intervals[i][1];
+            if(news<=tmpe){
+                result.back()[1] = max(tmpe,newe);
             }else{
                 result.push_back(intervals[i]);
             }
         }
         return result;
-     }
+    }
 };
