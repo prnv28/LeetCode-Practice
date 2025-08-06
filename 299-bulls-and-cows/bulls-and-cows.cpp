3db9fd84dcaa1812278a5fe0e6b08745d3d1fb2a
Pranav Gajera <pranavgajera1998@gmail.com>
2025-08-06 19:45:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.69%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
+    void printV(vector<int>& V){
+        for(int v:V){
+            cout<<v<<" ";
+        }
+        cout<<endl;
+    }
     string getHint(string secret, string guess) {
         int n = secret.size();
         int bulls = 0;
         int cows = 0;
-        map<char,pair<int,int>> mp;
-        for(int i=0;i<n;i++){
+        // map<char,pair<int,int>> mp;
+        vector<int> sec(10,0);
+        vector<int> gue(10,0);
+         for(int i=0;i<n;i++){
             if(secret[i]==guess[i]){
                 bulls++;
             }else{
-                if(mp.find(secret[i])==mp.end()){
-                    mp[secret[i]] = {1,0};
-                }else{
-                    mp[secret[i]].first++;
-                }
-
-                if(mp.find(guess[i])==mp.end()){
-                    mp[guess[i]] = {0,1};
-                }else{
-                    mp[guess[i]].second++;
-                }
+                sec[secret[i]-'0']++;
+                gue[guess[i]-'0']++;
             }
         }
-        for(auto it : mp){
-            cows += min(it.second.first,it.second.second);
+        // printV(sec);
+        // printV(gue);
+        // cout<<n<<endl;
+        for(int i=0;i<10;i++){
+            cows+=min(sec[i],gue[i]);
+            // cout<<cows<<" : "<<i<<endl;
         }
 
         return to_string(bulls)+"A"+to_string(cows)+"B";
