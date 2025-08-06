4578419dc89f2c2af87da5dda9710e5922f66f64
Pranav Gajera <pranavgajera1998@gmail.com>
2025-08-06 19:29:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (68.34%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -6,7 +6,6 @@ public:
         int ans = 0;
         while(left<=right){
             int mid = (left+right)/2;
-            cout<<mid<<endl;
             long long k = (long long ) mid * (mid+1)/2;
              if(k>n){
                 right = mid-1;
